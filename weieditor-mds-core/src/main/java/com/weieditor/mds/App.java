2c9a244b13f8b6fe8eb92088ce69bf0c51716589
Gao Rui <gaoruinan@163.com>
2013-08-19 20:52:44 +0800

    Init project structure.

---
+++ weieditor-mds-core/src/main/java/com/weieditor/mds/App.java
@@ -0,0 +1,13 @@
+package com.weieditor.mds;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
